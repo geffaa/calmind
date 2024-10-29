14796a909a3aa956c252c1c6177f44892fc6f038
Yodhimas Geffananda <yodhimas02@gmail.com>
2024-10-29 15:03:23 +0700

    fix(logo): fix logo directory

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'calmind-nine.vercel.app',
+      },
+    ],
+    unoptimized: true, // Menonaktifkan optimisasi gambar
+    minimumCacheTTL: 60,
+  },
 };
 
 export default nextConfig;
